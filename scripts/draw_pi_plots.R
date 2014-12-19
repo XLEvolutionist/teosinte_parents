1f4c5a4adc167336d4bc4f94e9f6ec626d5f3d0d
Simon Renny-Byfield <simonrenny-byfield@Jeffreys-iMac.local>
2014-12-18 19:20:08 -0800

    edits to plotting script

--- scripts/draw_pi_plots.R
+++ scripts/draw_pi_plots.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 #load in the the Tajima'ss D data, one table for each phase
-TD5<-read.table("/Users/simonrenny-byfield/test_angst/5/teosinte20thetas.gz.pestPG",header=T)
-TD10<-read.table("/Users/simonrenny-byfield/test_angst/10/teosinte20thetas.gz.pestPG",header=T)
-TD15<-read.table("/Users/simonrenny-byfield/test_angst/15/teosinte20thetas.gz.pestPG",header=T)
-TD20<-read.table("/Users/simonrenny-byfield/test_angst/20/teosinte20thetas.gz.pestPG",header=T)
+TD5<-read.table("/Users/simonrenny-byfield/test_angst/5/teosinte20thetas.gz.pestPG",header=T, skip = 1)
+TD10<-read.table("/Users/simonrenny-byfield/test_angst/10/teosinte20thetas.gz.pestPG",header=T, skip = 1)
+TD15<-read.table("/Users/simonrenny-byfield/test_angst/15/teosinte20thetas.gz.pestPG",header=T, skip = 1)
+TD20<-read.table("/Users/simonrenny-byfield/test_angst/20/teosinte20thetas.gz.pestPG",header=T, skip =1)
 
 #plot a 2x2 plot for each of the conditions, first plot Tajima's D
 
@@ -20,7 +20,7 @@ scatter.smooth(TD20[,14]/5000,TD20[,9], pch = 16, cex=.5, xlab="proportion oa ba
 
 #now plot 2x2 for pi
 #for 5 
-ylim=c(0,0.1)
+ylim=c(0,0.15)
 scatter.smooth(TD5[,14]/5000,(TD5[,5]/TD5[,14]), pch = 16, cex=.5, xlab="proportion of bases covered", ylab = "pi", col = "red", main="min 5 individuals",ylim=ylim)
 #for 10 
 scatter.smooth(TD10[,14]/5000,(TD10[,5]/TD10[,14]), pch = 16, cex=.5, xlab="proportion of bases covered", ylab = "pi", col = "red", main="min 10 individuals",ylim=ylim)
